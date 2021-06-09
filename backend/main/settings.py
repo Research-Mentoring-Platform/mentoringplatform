c68b3ae935d24e06afb6fa7c81057f6f4a1b25d4
Prince <prince17080@iiitd.ac.in>
2021-06-10 01:14:34 +0530

    Add JWT Token support

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -181,3 +181,13 @@ LOGGING = {
         }
     },
 }
+
+# Django REST framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
