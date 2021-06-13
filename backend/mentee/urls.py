61ef0cbd3e662c30bad90a3d1f9064738a030ada
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2021-06-13 23:19:46 +0530

    Add Mentee-app urls

--- backend/mentee/urls.py
+++ backend/mentee/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import MenteeViewSet
+from .views import MenteeViewSet, MenteeDepartmentViewSet, MenteeDesignationViewSet, MenteeDisciplineViewSet
 
 router = routers.DefaultRouter()
 router.register('mentee', MenteeViewSet)
+router.register('department', MenteeDepartmentViewSet)
+router.register('designation', MenteeDesignationViewSet)
+router.register('discipline', MenteeDisciplineViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
