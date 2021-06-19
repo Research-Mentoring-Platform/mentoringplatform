f5571e1287a1189302f05aeac03acc04daa60f16
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2021-06-19 17:48:26 +0530

    User permissions done

--- backend/main/mixins.py
+++ backend/main/mixins.py
@@ -1,23 +1,12 @@
 class ViewSetPermissionByMethodMixin:
     def get_permissions(self):
         try:
-            return [
-                permission()
-                for permission in self.permission_action_classes[self.action]
-            ]
+            return [permission() for permission in self.permission_action_classes[self.action]]
         except KeyError:
+            permission_classes = None
             if self.action:
                 action_func = getattr(self, self.action, {})
                 action_func_kwargs = getattr(action_func, 'kwargs', {})
-                permission_classes = action_func_kwargs.get(
-                    'permission_classes'
-                )
-            else:
-                permission_classes = None
+                permission_classes = action_func_kwargs.get('permission_classes')
 
-            return [
-                permission()
-                for permission in (
-                        permission_classes or self.permission_classes
-                )
-            ]
+            return [permission() for permission in (permission_classes or self.permission_classes)]
