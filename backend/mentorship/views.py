09ae11b0c7bd1ddeafadaaa591f5197968ded7e5
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2021-06-19 20:12:37 +0530

    Add basic CRUD API endpoints for MentorshipRequest model

--- backend/mentorship/views.py
+++ backend/mentorship/views.py
@@ -1,1 +1,8 @@
+from rest_framework import viewsets
 
+from mentorship.models import MentorshipRequest
+
+
+class MentorshipRequestViewSet(viewsets.ModelViewSet):
+    queryset = MentorshipRequest.objects.all()
+    lookup_field = 'uid'
