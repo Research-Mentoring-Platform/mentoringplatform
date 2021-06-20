33cd8a2ffc321ee2dd0c40c8049eaec059270c3a
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2021-06-20 23:59:06 +0530

    Complete functionality for MentorshipRequest APIs

--- backend/mentorship/serializers.py
+++ backend/mentorship/serializers.py
@@ -1,8 +1,19 @@
+from rest_framework import exceptions as rest_exceptions
 from rest_framework import serializers
 
 from mentee.models import Mentee
 from mentor.models import Mentor
-from mentorship.models import MentorshipRequest
+from mentorship.models import MentorshipRequest, MentorshipRequestStatus, Mentorship
+
+
+class MentorshipSerializer(serializers.ModelSerializer):
+    mentor = serializers.SlugRelatedField(slug_field='uid',
+                                          queryset=Mentor.objects.all(),
+                                          read_only=True)
+
+    mentee = serializers.SlugRelatedField(slug_field='uid',
+                                          queryset=Mentee.objects.all(),
+                                          read_only=True)
 
 
 class MentorshipRequestSerializer(serializers.ModelSerializer):
@@ -22,3 +33,32 @@ class MentorshipRequestSerializer(serializers.ModelSerializer):
         model = MentorshipRequest
         exclude = ('id',)
         read_only_fields = ('uid', 'status', 'reject_reason', 'date')
+
+
+class MentorshipRequestAcceptanceSerializer(serializers.ModelSerializer):
+    accepted = serializers.BooleanField(required=True)
+
+    class Meta:
+        model = MentorshipRequest
+        fields = ('accepted', 'reject_reason')
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        if not data['accepted'] and attrs['reject_reason'] == '':
+            raise rest_exceptions.ValidationError('Provide a reject_reason for rejection.')
+        if data['accepted'] and attrs['reject_reason'] != '':
+            raise rest_exceptions.ValidationError('Reject reason not needed for acceptance.')
+        return data
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        if validated_data['accepted']:
+            instance.status = MentorshipRequestStatus.REQUEST_ACCEPTED
+            Mentorship.objects.create(mentor=instance.mentor, mentee=instance.mentee)
+        else:
+            instance.status = MentorshipRequestStatus.REQUEST_REJECTED
+            instance.reject_reason = validated_data['reject_reason']
+        instance.save()
+        return instance
