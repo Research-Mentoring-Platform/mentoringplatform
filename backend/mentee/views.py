31234732fe0b190ec347124d2c61f044b0a158bf
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2021-07-02 21:25:57 +0530

    Finalise mentee-module validations and permissions.

--- backend/mentee/views.py
+++ backend/mentee/views.py
@@ -1,10 +1,9 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import exceptions as rest_exceptions
+from rest_framework import exceptions as rest_exceptions, status
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
 
 from main.mixins import ViewSetPermissionByMethodMixin
 from mentee import permissions as mentee_permissions
-from mentee.filters import MenteeEducationFilter, MenteeResearchFilter
 from mentee.models import Mentee, MenteeDesignation, MenteeDepartment, MenteeDiscipline, MenteeEducation, MenteeResearch
 from mentee.serializers import MenteeSerializer, MenteeDepartmentSerializer, MenteeDisciplineSerializer, \
     MenteeDesignationSerializer, MenteeEducationSerializer, MenteeResearchSerializer
@@ -66,32 +65,79 @@ class MenteeEducationViewSet(ViewSetPermissionByMethodMixin, viewsets.ModelViewS
     serializer_class = MenteeEducationSerializer
     queryset = MenteeEducation.objects.all()
     lookup_field = 'uid'
-    filter_backends = (DjangoFilterBackend,)
-    filterset_class = MenteeEducationFilter
 
     def get_queryset(self):
-        if self.action == 'list':
-            pass  # TODO continue from here
+        if self.action != 'list':
+            return super().get_queryset()
+
+        mentee = Mentee.objects.get(uid=self.request.query_params['mentee'])
+        if self.request.user.is_mentor:
+            if mentee_permissions.CanRetrieveMentee().has_object_permission(self.request, self, mentee):
+                return MenteeEducation.objects.filter(mentee=mentee)
+
+            raise rest_exceptions.PermissionDenied('You cannot view the education list of the requested mentee.')
+
+        elif self.request.user.is_mentee:
+            if self.request.user == mentee.user:
+                return MenteeEducation.objects.filter(mentee=mentee)
+
+            raise rest_exceptions.PermissionDenied('You cannot view the education list of the requested mentee.')
 
     def list(self, request, *args, **kwargs):
         """
-        The filterset_class enforces it to have the query-parameter: mentee
+        This function validates and returns list depending upon whether the requesting user is a mentor or a mentee.
+        The validation checks and filtering is done in overridden get_queryset() method.
         """
-        ret = super().list(request, *args, **kwargs)
-        if request.user.is_mentor:
-            mentee = Mentee.objects.get(uid=request.query_params.mentee)
-            if mentee_permissions.CanRetrieveMentee().has_object_permission(request, self, mentee):
-                return MenteeEducation.objects.filter(mentee=mentee)
-            raise rest_exceptions.PermissionDenied('You cannot view the education list of the requested mentee.')
-        elif request.user.is_mentee:
-            return MenteeEducation.objects.filter()
-        raise rest_exceptions.PermissionDenied('You cannot view')
+
+        if 'mentee' not in request.query_params:
+            return Response(data=dict(mentee='You should provide the `mentee` query parameter.'),
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if not Mentee.objects.filter(uid=request.query_params['mentee']).exists():
+            return Response(data=dict(mentee='Invalid mentee UID provided.'), status=status.HTTP_400_BAD_REQUEST)
+
+        return super().list(request, *args, **kwargs)
 
 
-class MenteeResearchViewSet(viewsets.ModelViewSet):
-    permission_classes = (permissions.IsAuthenticated,)
+class MenteeResearchViewSet(ViewSetPermissionByMethodMixin, viewsets.ModelViewSet):
+    permission_classes = (mentee_permissions.CanAccessMenteeResearch,)
+    permission_action_classes = dict(
+        create=(mentee_permissions.IsMentee,),
+        retrieve=(mentee_permissions.CanRetrieveMenteeResearch,),
+        list=(permissions.IsAuthenticated,)
+    )
     serializer_class = MenteeResearchSerializer
     queryset = MenteeResearch.objects.all()
     lookup_field = 'uid'
-    filter_backends = (DjangoFilterBackend,)
-    filterset_class = MenteeResearchFilter
+
+    def get_queryset(self):
+        if self.action != 'list':
+            return super().get_queryset()
+
+        mentee = Mentee.objects.get(uid=self.request.query_params['mentee'])
+        if self.request.user.is_mentor:
+            if mentee_permissions.CanRetrieveMentee().has_object_permission(self.request, self, mentee):
+                return MenteeResearch.objects.filter(mentee=mentee)
+
+            raise rest_exceptions.PermissionDenied('You cannot view the education list of the requested mentee.')
+
+        elif self.request.user.is_mentee:
+            if self.request.user == mentee.user:
+                return MenteeResearch.objects.filter(mentee=mentee)
+
+            raise rest_exceptions.PermissionDenied('You cannot view the education list of the requested mentee.')
+
+    def list(self, request, *args, **kwargs):
+        """
+        This function validates and returns list depending upon whether the requesting user is a mentor or a mentee.
+        The validation checks and filtering is done in overridden get_queryset() method.
+        """
+
+        if 'mentee' not in request.query_params:
+            return Response(data=dict(mentee='You should provide the `mentee` query parameter.'),
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if not Mentee.objects.filter(uid=request.query_params['mentee']).exists():
+            return Response(data=dict(mentee='Invalid mentee UID provided.'), status=status.HTTP_400_BAD_REQUEST)
+
+        return super().list(request, *args, **kwargs)
