2e3d26b96fdc608b05f342f89c872e6801c01d23
s-gup <55698588+s-gup@users.noreply.github.com>
2021-09-19 20:06:48 +0530

    Created user test data file

---
+++ backend/users/test_data.py
@@ -0,0 +1,21 @@
+VALID_USER_DATA = {
+    'username': 'username',
+    'password': 'pass@1234',
+    'email': 'name@abc.de',
+    'first_name': 'first_name',
+    'last_name': 'last_name',
+    'date_of_birth': '2001-08-07',
+    'is_mentor': True,
+    'is_mentee': False,
+}
+
+VALID_USER_DATA2 = {
+    'username': 'name',
+    'password': 'pass@4321',
+    'email': 'name@def.gh',
+    'first_name': 'first_name',
+    'last_name': 'last_name',
+    'date_of_birth': '2003-08-07',
+    'is_mentor': False,
+    'is_mentee': True,
+}
