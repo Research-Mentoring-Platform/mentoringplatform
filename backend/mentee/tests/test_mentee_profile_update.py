ac8d34a470b0c662c7f3dc1c08c6915fe252bdeb
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-09-23 00:24:30 +0530

    test: Add tests to check authorized mentee profile updates

--- backend/mentee/tests/test_mentee_profile_update.py
+++ backend/mentee/tests/test_mentee_profile_update.py
@@ -4,7 +4,7 @@ import logging
 from django.test import TestCase
 from rest_framework import status
 from users.models import CustomUser
-from mentee.models import Mentee
+from mentee.models import Mentee, MenteeDepartment, MenteeDesignation, MenteeDiscipline
 
 class MenteeProfileUpdateTestCase(TestCase):
     @classmethod
@@ -20,9 +20,41 @@ class MenteeProfileUpdateTestCase(TestCase):
         return super().tearDownClass()
 
     def setUp(self):
-        x = self.client.login(email=self.login_user['email'], password=self.login_user['password'])
-        self.user = CustomUser.objects.get(email=self.login_user['email'])
-        print(self.user.mentee)
+        # log in the mentee and get the logged in user object
+        self.client.login(email=self.login_user['email'], password=self.login_user['password'])
+
+        # get the mentee object for the logged in user
+        self.mentee = CustomUser.objects.get(email=self.login_user['email']).mentee
+
+        # get the mentee designation, department and discipline uids
+        desig_uid = MenteeDesignation.objects.get(label='Industry Researcher').uid
+        deptt_uid = MenteeDepartment.objects.get(label='Computer Science and Design').uid
+        disc_uid= MenteeDiscipline.objects.get(label='Human-Computer Interaction').uid
+
+        # # construct a profile data object which contains all the mentee fields to be modified
+        self.data = {
+            'about_self': 'Fugiat ad id ut ullamco commodo irure duis reprehenderit reprehenderit irure non in ex Lorem.',
+            'department': deptt_uid,        # mandatory field
+            'discipline': disc_uid,         # mandatory field
+            'designation': desig_uid,       # mandatory field
+            'specialization': 'lorem ipsum',
+        }
 
     def test_mentee_updates_own_profile(self):
-        pass
+        """ tests the response when mentee updates own profile """
+        m_uid = self.mentee.uid
+        response = self.client.put(f'/api/mentee/mentee/{m_uid}', data=self.data, content_type='application/json', follow=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_mentee_updates_different_profile(self):
+        """ tests the response when mentee updates a different mentee's profile """
+        m_user = CustomUser.objects.get(email='karan17058@iiitd.ac.in')
+        m_uid = m_user.mentee.uid
+        response = self.client.put(f'/api/mentee/mentee/{m_uid}', data=self.data, content_type='application/json', follow=True)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_mentee_updates_invalid_profile(self):
+        """ tests the response when mentee updates an invalid profile """
+        m_uid = "abcdef"
+        response = self.client.put(f'/api/mentee/mentee/{m_uid}', data=self.data, content_type='application/json', follow=True)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
