ec7e207e2695ee2976aa8037665ce95cb103043a
bhavesh <bhavesh19355@iiitd.ac.in>
2021-10-07 01:55:35 +0530

    made changes in user_login_test

--- backend/users/tests/test_user_login.py
+++ backend/users/tests/test_user_login.py
@@ -2,21 +2,59 @@ import logging
 from django.test.testcases import TestCase
 from users.models import CustomUser
 
+Test_User_1 = {
+    'username' : 'test_user1',
+    'email' : 'hello@gmail.com',
+    'first_name' : 'test_mentor',
+    'password':'pass4321',
+    'last_name' : 'last_name',
+    'date_of_birth': '2001-08-07',
+    'is_mentor': True,
+    'email_verified' : True
+}
+Test_User_2 = {
+    'username' : 'test_user2',
+    'email' : 'hello123@gmail.com',
+    'first_name' : 'test_mentee',
+    'password':'pass4321',
+    'last_name' : 'last_name',
+    'date_of_birth': '2001-08-07',
+    'is_mentee': True,
+    'email_verified' : True
+}
+Test_User_3= {
+    'username':'test_user3',
+    'password':'pass4321',
+    'email':'hello456@gmail.com',
+    'first_name':'unverify_test_mentor',
+    'last_name':'last_name',
+    'date_of_birth':'2001-08-07',
+    'is_mentor':True
+}
+Test_User_4 ={
+    'username':'test_user4',
+    'password':'pass4321',
+    'email':'hello789@gmail.com',
+    'first_name':'unverify_test_mentee',
+    'last_name':'last_name',
+    'date_of_birth':'2001-08-07',
+    'is_mentee':True
+}
 class LoginTestCases(TestCase):
     @classmethod
     def setUpClass(cls):
         logging.disable(logging.CRITICAL)
-        cls.test_mentor=CustomUser(username='test_user1',email='hello@gmail.com',first_name='test_mentor',last_name='last_name',date_of_birth='2001-08-07',is_mentor=True,email_verified=True)
-        cls.test_mentor.set_password("pass4321")
+        cls.test_mentor=CustomUser(**Test_User_1)
+        cls.test_mentor.set_password(Test_User_1['password'])
         cls.test_mentor.save()
-        cls.test_mentee=CustomUser(username='test_user2',password='pass4321',email='hello123@gmail.com',first_name='test_mentee',last_name='last_name',date_of_birth='2001-08-07',is_mentee=True,email_verified=True)
-        cls.test_mentee.set_password("pass4321")
+        cls.test_mentee=CustomUser(**Test_User_2)
+        cls.test_mentee.set_password(Test_User_2['password'])
         cls.test_mentee.save()
-        cls.unverified_test_mentor=CustomUser(username='test_user3',password='pass4321',email='hello456@gmail.com',first_name='unverify_test_mentor',last_name='last_name',date_of_birth='2001-08-07',is_mentor=True)
-        cls.unverified_test_mentor.set_password("pass4321")
+        cls.unverified_test_mentor=CustomUser(**Test_User_3)
+        cls.unverified_test_mentor.set_password(Test_User_3['password'])
         cls.unverified_test_mentor.save()
-        cls.unverified_test_mentee=CustomUser(username='test_user4',password='pass4321',email='hello789@gmail.com',first_name='unverify_test_mentee',last_name='last_name',date_of_birth='2001-08-07',is_mentee=True)
-        cls.unverified_test_mentee.set_password("pass4321")
+        cls.unverified_test_mentee=CustomUser(**Test_User_4)
+        cls.unverified_test_mentee.set_password(Test_User_4['password'])
         cls.unverified_test_mentee.save()
         super().setUpClass()
        
@@ -32,7 +70,7 @@ class LoginTestCases(TestCase):
     
     def test_verified_mentor_login(self):
         data={
-            'email' : LoginTestCases.test_mentor.email, 
+            'email' : self.test_mentor.email, 
             'password' : 'pass4321'
         }
         response=self.client.post('/api/users/token/',data=data)
@@ -40,7 +78,7 @@ class LoginTestCases(TestCase):
 
     def test_verified_mentee_login(self):
         data={
-            'email' : LoginTestCases.test_mentee.email,
+            'email' : self.test_mentee.email,
             'password' : 'pass4321'
         }
         response=self.client.post('/api/users/token/',data=data)
@@ -48,7 +86,7 @@ class LoginTestCases(TestCase):
         
     def test_unverified_mentor_login(self):
         data={
-            'email' : LoginTestCases.unverified_test_mentor.email,
+            'email' : self.unverified_test_mentor.email,
             'password' : 'pass4321'
         }
         response=self.client.post('/api/users/token/',data=data)
@@ -56,7 +94,7 @@ class LoginTestCases(TestCase):
         
     def test_unverified_mentee_login(self):
         data={
-            'email' : LoginTestCases.unverified_test_mentee.email,
+            'email' : self.unverified_test_mentee.email,
             'password' : 'pass4321'
         }
         response=self.client.post('/api/users/token/',data=data)
