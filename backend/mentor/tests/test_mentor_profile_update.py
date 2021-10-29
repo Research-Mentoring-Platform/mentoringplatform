5ca48cf9255d74dd4b73a13aadf3f1cf87e74a62
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-10-29 20:29:11 +0530

    test: fix DB update issue and make code MUCH neater

--- backend/mentor/tests/test_mentor_profile_update.py
+++ backend/mentor/tests/test_mentor_profile_update.py
@@ -1,11 +1,11 @@
-import json
-import logging
+import json, uuid, logging, random
 
+from django.db.models import Q
 from django.test import TestCase
 from rest_framework import status
 from users.models import CustomUser
-from mentor.models import Mentor, MentorDepartment, MentorDesignation, MentorDiscipline
-from mentee.models import Mentee, MenteeDepartment, MenteeDesignation, MenteeDiscipline
+from mentor.models import MentorDepartment, MentorDesignation, MentorDiscipline
+from mentee.models import MenteeDepartment, MenteeDesignation, MenteeDiscipline
 
 
 class MentorProfileUpdateTestCase(TestCase):
@@ -15,7 +15,7 @@ class MentorProfileUpdateTestCase(TestCase):
         # select a mentor from the test_users.json list to login for tests
         with open('config/test_users.json') as f:
             users_data = json.load(f)['users']
-            cls.login_user = next(user for user in users_data if user['email'] ==  'prince17080@iiitd.ac.in')
+            cls.login_user = next(user for user in users_data if user['email'] == 'prince17080@iiitd.ac.in')
         logging.disable(logging.CRITICAL)
 
     @classmethod
@@ -29,10 +29,10 @@ class MentorProfileUpdateTestCase(TestCase):
         # get the mentor object for the logged in user
         self.mentor = CustomUser.objects.get(email=self.login_user['email']).mentor
 
-        # get the mentor designation, department and discipline uids
-        desig_uid = MentorDesignation.objects.get(label='Industry Researcher').uid
-        deptt_uid = MentorDepartment.objects.get(label='Computer Science and Design').uid
-        disc_uid= MentorDiscipline.objects.get(label='Human-Computer Interaction').uid
+        # get a designation, department, discipline that are different from the mentor's values
+        desig_uid = next(d for d in list(MentorDesignation.objects.all()) if d.uid != self.mentor.designation).uid
+        deptt_uid = next(d for d in list(MentorDepartment.objects.all()) if d.uid != self.mentor.department).uid
+        disc_uid = next(d for d in list(MentorDiscipline.objects.all()) if d.uid != self.mentor.discipline).uid
 
         # construct a profile data object which contains all the mentor fields to be modified
         self.data = {
@@ -41,78 +41,95 @@ class MentorProfileUpdateTestCase(TestCase):
             'discipline': disc_uid,         # mandatory field
             'designation': desig_uid,       # mandatory field
             'specialization': 'lorem ipsum',
-            'expected_min_mentorship_duration': 2,
-            'expected_max_mentorship_duration': 4,
-            'is_accepting_mentorship_requests': False
         }
 
     def test_mentor_updates_own_profile(self):
         """ tests the response when mentor updates own profile """
         m_uid = self.mentor.uid
+
+        # mentor's data before the PATCH request
+        prev_data = {
+            'about_self': self.mentor.about_self,
+            'department': self.mentor.department.uid,
+            'designation': self.mentor.designation.uid,
+            'discipline': self.mentor.discipline.uid,
+            'specialization': self.mentor.specialization,
+        }
+
         response = self.client.patch(f'/api/mentor/mentor/{m_uid}/', data=self.data, content_type='application/json', follow=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # print(response.data)
-        mentor = Mentor.objects.get(uid=m_uid)
+        self.mentor.refresh_from_db()
 
-        self.assertEqual(mentor.about_self, self.data['about_self'])
-        self.assertEqual(mentor.department.uid, self.data['department'])
-        self.assertEqual(mentor.discipline.uid, self.data['discipline'])
-        self.assertEqual(mentor.designation.uid, self.data['designation'])
-        self.assertEqual(mentor.specialization, self.data['specialization'])
-        self.assertEqual(mentor.expected_min_mentorship_duration, self.data['expected_min_mentorship_duration'])
-        self.assertEqual(mentor.expected_max_mentorship_duration, self.data['expected_max_mentorship_duration'])
-        self.assertEqual(mentor.is_accepting_mentorship_requests, self.data['is_accepting_mentorship_requests'])
+        self.assertEqual(self.mentor.about_self, prev_data['about_self'])
+        self.assertEqual(self.mentor.department.uid, prev_data['department'])
+        self.assertEqual(self.mentor.discipline.uid, prev_data['discipline'])
+        self.assertEqual(self.mentor.designation.uid, prev_data['designation'])
+        self.assertEqual(self.mentor.specialization, prev_data['specialization'])
 
     def test_mentor_updates_different_profile(self):
         """ tests the response when mentor updates a different mentor's profile """
         m_user = CustomUser.objects.get(email='ananya17020@iiitd.ac.in')
-        m_uid = m_user.mentor.uid
-        response = self.client.patch(f'/api/mentor/mentor/{m_uid}/', data=self.data, content_type='application/json', follow=True)
+        diff_mentor = m_user.mentor
+
+        # mentor's data before the PATCH request
+        prev_data = {
+            'about_self': diff_mentor.about_self,
+            'department': diff_mentor.department.uid,
+            'designation': diff_mentor.designation.uid,
+            'discipline': diff_mentor.discipline.uid,
+            'specialization': diff_mentor.specialization,
+        }
+
+        response = self.client.patch(f'/api/mentor/mentor/{diff_mentor.uid}/', data=self.data, content_type='application/json', follow=True)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        diff_mentor.refresh_from_db()
 
-        diff_mentor = Mentor.objects.get(uid=m_uid)
-        self.assertNotEqual(diff_mentor.about_self, self.data['about_self'])
-        if m_user.mentor.department.uid != self.data['department']:
-            self.assertNotEqual(diff_mentor.department.uid, self.data['department'])
-        if m_user.mentor.discipline.uid != self.data['discipline']:
-            self.assertNotEqual(diff_mentor.discipline.uid, self.data['discipline'])
-        if m_user.mentor.designation.uid != self.data['designation']:
-            self.assertNotEqual(diff_mentor.designation.uid, self.data['designation'])
-        self.assertNotEqual(diff_mentor.specialization, self.data['specialization'])
-        if m_user.mentor.expected_min_mentorship_duration != self.data['expected_min_mentorship_duration']:
-            self.assertNotEqual(diff_mentor.expected_min_mentorship_duration, self.data['expected_min_mentorship_duration'])
-        if m_user.mentor.expected_max_mentorship_duration != self.data['expected_max_mentorship_duration']:
-            self.assertNotEqual(diff_mentor.expected_max_mentorship_duration, self.data['expected_max_mentorship_duration'])
-        if m_user.mentor.is_accepting_mentorship_requests != self.data['is_accepting_mentorship_requests']:
-            self.assertNotEqual(diff_mentor.is_accepting_mentorship_requests, self.data['is_accepting_mentorship_requests'])
-
+        self.assertEqual(diff_mentor.about_self, prev_data['about_self'])
+        self.assertEqual(diff_mentor.department.uid, prev_data['department'])
+        self.assertEqual(diff_mentor.discipline.uid, prev_data['discipline'])
+        self.assertEqual(diff_mentor.designation.uid, prev_data['designation'])
+        self.assertEqual(diff_mentor.specialization, prev_data['specialization'])
 
     def test_mentor_updates_invalid_profile(self):
         """ tests the response when mentor updates an invalid profile """
-        m_uid = "abcdef"
+        m_uid = uuid.uuid4()
         response = self.client.patch(f'/api/mentor/mentor/{m_uid}/', data=self.data, content_type='application/json', follow=True)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_mentor_updates_mentee_profile(self):
         """ tests the response when mentor updates a mentee's profile """
         m_user = CustomUser.objects.get(email='karan17058@iiitd.ac.in')
-        m_uid = m_user.mentee.uid
+        mentee = m_user.mentee
+        m_uid = mentee.uid
+
+        # mentee's data before the PATCH request
+        prev_data = {
+            'about_self': mentee.about_self,
+            'department': mentee.department.uid,
+            'discipline': mentee.discipline.uid,
+            'designation': mentee.designation.uid,
+            'specialization': mentee.specialization,
+        }
+
+        # get a designation, department, discipline that are different from the mentee's values
+        desig_uid = next(d for d in list(MenteeDesignation.objects.all()) if d.uid != mentee.designation).uid
+        deptt_uid = next(d for d in list(MenteeDepartment.objects.all()) if d.uid != mentee.department).uid
+        disc_uid = next(d for d in list(MenteeDepartment.objects.all()) if d.uid != mentee.discipline).uid
+
         mentee_data = {
             'about_self': self.data['about_self'],
-            'department': MenteeDepartment.objects.get(label='Computer Science and Design').uid,
-            'discipline': MenteeDiscipline.objects.get(label='Operating Systems').uid,
-            'designation': MenteeDesignation.objects.get(label='MTech').uid,
+            'department': deptt_uid,
+            'discipline': disc_uid,
+            'designation': desig_uid,
             'specialization': 'lorem ipsum',
         }
+
         response = self.client.patch(f'/api/mentee/mentee/{m_uid}/', data=mentee_data, content_type='application/json', follow=True)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        mentee.refresh_from_db()
 
-        mentee = Mentee.objects.get(uid=m_uid)
-        self.assertNotEqual(mentee.about_self, mentee_data['about_self'])
-        if m_user.mentee.department.uid != mentee_data['department']:
-            self.assertNotEqual(mentee.department.uid, mentee_data['department'])
-        if m_user.mentee.discipline.uid != mentee_data['discipline']:
-            self.assertNotEqual(mentee.discipline.uid, mentee_data['discipline'])
-        if m_user.mentee.designation.uid != mentee_data['designation']:
-            self.assertNotEqual(mentee.designation.uid, mentee_data['designation'])
-        self.assertNotEqual(mentee.specialization, mentee_data['specialization'])
+        self.assertEqual(mentee.about_self, prev_data['about_self'])
+        self.assertEqual(mentee.department.uid, prev_data['department'])
+        self.assertEqual(mentee.discipline.uid, prev_data['discipline'])
+        self.assertEqual(mentee.designation.uid, prev_data['designation'])
+        self.assertEqual(mentee.specialization, prev_data['specialization'])
